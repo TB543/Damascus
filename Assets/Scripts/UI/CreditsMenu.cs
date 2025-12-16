681d2ee9a8c3c4b775fce9b0bf7cb5eb8b16dca2
TB543 <tbarron543@gmail.com>
2025-12-16 18:01:03 -0500

    update to unity 6.3

--- Assets/Scripts/UI/CreditsMenu.cs
+++ Assets/Scripts/UI/CreditsMenu.cs
@@ -6,23 +6,24 @@ using UnityEngine.UIElements;
 public class AutoScroller : MonoBehaviour
 {
     [SerializeField] private float scrollSpeed = 50;
-    private VisualElement root;
+    private VisualElement frame;
+    private VisualElement button;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        root = GetComponent<UIDocument>().rootVisualElement;
-        Button menuButton = root.Q<Button>("MenuButton");
-        root = root.Q<VisualElement>("AutoScrollContainer");
-        menuButton.RegisterCallback<ClickEvent>(OnMenuButtonClicked);
+        frame = GetComponent<UIDocument>().rootVisualElement;
+        button = frame.Q<Button>("MenuButton");
+        frame = frame.Q<VisualElement>("AutoScrollContainer");
+        button.RegisterCallback<ClickEvent>(OnMenuButtonClicked);
     }
 
     // Update is called once per frame
     void Update()
     {
-        root.transform.position += Vector3.down * Time.deltaTime * scrollSpeed;
-        if (-root.transform.position.y > root.resolvedStyle.height)
-            root.transform.position = Vector3.zero;
+        frame.style.translate = frame.resolvedStyle.translate + (Vector3.down * Time.deltaTime * scrollSpeed);
+        if (-frame.resolvedStyle.translate.y > frame.resolvedStyle.height + button.resolvedStyle.translate.y + (button.resolvedStyle.height * 2))
+            frame.style.translate = Vector3.zero;
     }
 
     private void OnMenuButtonClicked(ClickEvent evt)
